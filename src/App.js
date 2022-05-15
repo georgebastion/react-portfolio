5808e1a35462353aa90bc2db331311032c0f7c69
georgebastion <georgejacob437@gmail.com>
2022-05-15 21:42:36 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./globalStyles"
+import { lightTheme } from "./components/Themes"
+import { darkTheme } from "./components/Themes"
+
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+  <GlobalStyle/>
+
+  <ThemeProvider theme={darkTheme}>
+  App file
+  </ThemeProvider>
+  
+  </>
     
 }
 
